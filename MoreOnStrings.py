412037a148381d21a8aa055164d4de9e45698887
rishabh gupta <rishabh.gupta@sita.aero>
2025-08-31 13:13:36 +0530

    Intro#2

---
+++ MoreOnStrings.py
@@ -0,0 +1,9 @@
+a1 = "Hello "\
+     "Boy"
+#automatically by pycharm
+a2 = 'Hello'
+a3 = '''Hello
+boy'''
+#Multi line string, will store new line as well
+
+print(a1,a2,a3)
---
+++ UserInput.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+print(name.capitalize())
+
+
+
+num = input("Enter your number: ") # always input as string
+print(type(num))
+print(int(num) + 6)
